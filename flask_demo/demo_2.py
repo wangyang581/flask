860dd8ab605f43e85c4d2dbfe7ddb3cd44d935e0
王朝兴 <wangzhx@zhejianglab.com>
2024-04-28 17:29:35 +0800

    4-28 17:29

--- flask_demo/demo_2.py
+++ flask_demo/demo_2.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -17,10 +17,20 @@ def profile():
     return "我是个人中心"
 
 
-# 定义有参数的url
-@app.route("/blog/<blog_id>")
+# 定义有参数的url,可以限制参数类型，定义方式是将参数固定到path中
+# 参数类型可以是string，int，float，path，uuid，any（any可以是上述类型中的任何一个）
+@app.route("/blog/<int:blog_id>")
 def blog_list(blog_id):
     return "你所访问的博客是：%s" % blog_id
+
+# book/list:该接口为书的第一页数据
+# book/list?page=2:这种方式可以获取第二页数据，http协议规定用?传参
+@app.route("/book/list")
+def book_list():
+    # arguments:参数
+    # request.args:类字典类型
+    page=request.args.get('page', default=1,type=int)
+    return "您获取的是第{}页".format(page)
 
 
 if __name__ == "__main__":
